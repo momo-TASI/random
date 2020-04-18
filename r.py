abf604cdb899a38e3909ba0ecea00fe466598960
Memzy Qin <ytzs5201314@gmail.com>
2020-04-18 21:14:38 +0800

    randint change

--- r.py
+++ r.py
@@ -1,6 +1,9 @@
 import random
-
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍開始值: ')
+end = input('請決定隨機數字範圍結束值: ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 i = 0
 while True:
 	i += 1 # i = i + 1
