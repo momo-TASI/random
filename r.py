79ba932654e6cdcc0e5056bb816d7521ba7ee609
Memzy Qin <ytzs5201314@gmail.com>
2020-04-18 21:08:40 +0800

    print(count)

--- r.py
+++ r.py
@@ -1,15 +1,17 @@
 import random
 
 r = random.randint(1, 100)
-i = 5
+i = 0
 while True:
-	i = i - 1
+	i += 1 # i = i + 1
 	rand = input ('請輸入數字: ')
 	rand = int(rand)
 	if rand == r :
 		print('恭喜你猜對了')
+		print('這是你猜的第' , i , '次')
 		break
 	elif rand > r:
 		print('比答案大')
 	elif rand < r:
 		print('比答案小')
+	print('這是你猜的第' , i , '次')
