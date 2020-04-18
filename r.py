5ce6bd074531b00b45cc8437949ef2583b6c2236
Memzy Qin <ytzs5201314@gmail.com>
2020-04-18 20:57:34 +0800

    add if > 0 ~~

--- r.py
+++ r.py
@@ -1,12 +1,17 @@
 import random
 
 r = random.randint(1, 100)
-
+i = 5
 while True:
+	i = i - 1
 	rand = input ('請輸入數字: ')
 	rand = int(rand)
 	if rand == r :
 		print('恭喜你猜對了')
 		break
 	else:
- 		print('再來一次')
+ 		print('猜錯囉')
+ 		if i > 0 :
+ 			print('還有' , i , '次機會')
+ 		if i == 0 :
+ 			break
